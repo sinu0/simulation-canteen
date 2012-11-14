a3b8236b7201b911bf8aebfa9376214e50e484d0
Mariusz Pacek <pacekmariusz.email@gmail.com>
2012-11-14 23:04:10 +0100

    first

---
+++ src/canteen/canteen.java
@@ -0,0 +1,40 @@
+package canteen;
+
+
+import desmoj.core.simulator.*;
+
+public class canteen extends Model {
+
+	public canteen(Model arg0, String arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return "Symulacja super stołowki";
+	}
+
+	@Override
+	public void doInitialSchedules() {
+		
+		
+	}
+
+	@Override
+	public void init() {
+		
+		
+	}
+
+}
---
+++ src/canteen/cook.java
@@ -0,0 +1,13 @@
+package canteen;
+
+import desmoj.core.simulator.Entity;
+import desmoj.core.simulator.Model;
+
+public class cook extends Entity {
+
+	public cook(Model arg0, String arg1, boolean arg2) {
+		super(arg0, arg1, arg2);
+		// TODO Auto-generated constructor stub
+	}
+
+}
