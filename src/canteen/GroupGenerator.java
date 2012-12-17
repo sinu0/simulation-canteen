c3ccd704ba57f1ac0609a978c243966eeb7ad1d7
Mariusz Pacek <pacekmariusz.email@gmail.com>
2012-12-17 20:50:43 +0100

    add Min and Max group

--- src/canteen/GroupGenerator.java
+++ src/canteen/GroupGenerator.java
@@ -10,10 +10,14 @@ import desmoj.core.simulator.TimeSpan;
 public class GroupGenerator extends ClientGenerator{
 	private Random rand;
 	private LinkedList<Client> groupOfClient;
+	private int groupMax;
+	private int groupMin;
 	public GroupGenerator(Model arg0, String arg1, boolean arg2) {
 		super(arg0, arg1, arg2);
 		groupOfClient = new LinkedList<Client>();
 		rand = new Random();
+		groupMax=4;
+		groupMin=2;
 	}
 
 	@Override
@@ -22,12 +26,12 @@ public class GroupGenerator extends ClientGenerator{
 			
 			groupClientGenerate++;
 			double numberOfClient = (1+rand.nextGaussian())*100;//guassian zwraca rozkład normalny w zakresie 0 - 100 gdzie wartosc oczekiwana to 50
-			if(numberOfClient>40 && numberOfClient<60) numberOfClient=2;
+			if(numberOfClient>40 && numberOfClient<60) numberOfClient=groupMin;
 			else
-			if(numberOfClient<40 && numberOfClient>0) numberOfClient=3;
+			if(numberOfClient<40 && numberOfClient>0) numberOfClient=(int)(groupMax+groupMin)/2;
 			else
 			if(numberOfClient>100 || numberOfClient<0) numberOfClient=0;
-				numberOfClient=4;
+				numberOfClient=groupMax;
 			double decisionPoints=0;
 			
 			for(int i=0;i<numberOfClient;i++){
@@ -57,5 +61,16 @@ public class GroupGenerator extends ClientGenerator{
 		}
 		
 	}
-
+	public int getGroupMax(){
+		return groupMax;
+	}
+	public int getGroupMin(){
+		return groupMax;
+	}
+	public void setGroupMax(int max){
+		this.groupMax=max;
+	}
+	public void setGroupMin(int min){
+		this.groupMax=min;
+	}
 }
