a145a8eb7c75e3f97525ac7e82bc852364c29682
Gaxit <gaxit@interia.eu>
2013-01-06 21:14:47 +0100

    The end?

--- src/gui/AnimPanel.java
+++ src/gui/AnimPanel.java
@@ -52,33 +52,45 @@ public class AnimPanel extends JPanel implements PropertyChangeListener
 	setLayout(null);  
 	tables = new LinkedList<JLabel>();
 	
-	//Utworzenie nowego pliku na dysku
     File imageFile = new File("gui.png");
-    //wczytanie obrazu z pliku
-    try {
-		diskImage = ImageIO.read(imageFile);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+    try
+    {
+	  diskImage = ImageIO.read(imageFile);
 	}
-    
+    catch (IOException e) {}
 	repaint();
   }
   
   public void paintComponent(Graphics g)
   {
-	
-      g.drawImage(diskImage, 0, 0, this);
+    g.drawImage(diskImage, 0, 0, this);
+    int j=0;
+    for (int i=0;i<model.getTable2Count();i++)
+    {
+      int k=j/5;
+      //if (i>4) j=1;
+        g.drawRect(100+(j-5*k)*130, 270+k*100, 80, 80);
+      j++;
+    }
+    for (int i=0;i<model.getTable4Count();i++)
+    {
+      //int j=0;
+    	int k=j/5;
+      //if (i>4) j=1;
+        g.drawRect(100+(j-5*k)*130, 270+k*100, 80, 80);
+      j++;
+    }
   }
   
   public void drawPanel()
   {
 	speedLabel = new JLabel();
-	speedLabel.setBounds(300, 0, 100, 20);
+	speedLabel.setBounds(650, 25, 100, 20);
 	add(speedLabel);
 	
-	speedSlider = new JSlider(1, 100, 50);
-	speedSlider.setBounds(300, 20, 200, 30);
+	final int max = 101;
+	speedSlider = new JSlider(1, max - 1, (max+max/4)/2);
+	speedSlider.setBounds(580, 0, 200, 30);
 	add(speedSlider);
 	speedSlider.addChangeListener(new ChangeListener() {
 		@Override
@@ -86,7 +98,7 @@ public class AnimPanel extends JPanel implements PropertyChangeListener
 			// TODO Auto-generated method stub
 			//System.out.println("ZMIENILEM WARTOSC");
 			speedLabel.setText("Predkosc - " + speedSlider.getValue());
-			model.setDelay(speedSlider.getValue());
+			model.setDelay(max - speedSlider.getValue());
 		}
 	});
 	speedLabel.setText("Predkosc - " + speedSlider.getValue());
@@ -94,62 +106,64 @@ public class AnimPanel extends JPanel implements PropertyChangeListener
 	
 	
     idleCook = new JLabel("Wolni kucharze: " + model.getCookCount());
-	idleCook.setBounds(100, 50, 140, 20);
+	idleCook.setBounds(50, 40, 140, 20);
 	add(idleCook);
 	    
 	workingCook = new JLabel("Pracujï¿½cy kucharze: 0");
-	workingCook.setBounds(100, 80, 150, 20);
+	workingCook.setBounds(50, 70, 150, 20);
 	add(workingCook);
 	    
-	cashier = new JLabel("Kasjerzy: " + model.getCashierCount());
-	cashier.setBounds(350, 150, 120, 20);
+	cashier = new JLabel("Kasjerki: " + model.getCashierCount());
+	cashier.setBounds(500, 100, 120, 20);
 	add(cashier);
 	    
-	clientQueue = new JLabel("Kolejka klientï¿½w: 0");
-	clientQueue.setBounds(310, 180, 200, 20);
+	clientQueue = new JLabel("Kolejka klientów: 0");
+	clientQueue.setBounds(480, 200, 200, 20);
 	add(clientQueue);
 	    
 	ingredients = new JLabel("Skladniki:");
-	ingredients.setBounds(550, 10, 100, 20);
+	ingredients.setBounds(270, 0, 100, 20);
 	add(ingredients);
 	   
 	kotlet = new JLabel("Kotlety: 5");
-	kotlet.setBounds(560, 30, 100, 20);
+	kotlet.setBounds(280, 15, 100, 20);
 	add(kotlet);
 	    
 	wolowina = new JLabel("Wolowina: 5");
-	wolowina.setBounds(560, 50, 100, 20);
+	wolowina.setBounds(280, 30, 100, 20);
 	add(wolowina);    
 	    
 	ziemniaki = new JLabel("Ziemniaki: 5");
-	ziemniaki.setBounds(560, 70, 100, 20);
+	ziemniaki.setBounds(280, 45, 100, 20);
 	add(ziemniaki);    
 	    
 	ryz = new JLabel("Ryz: 5");
-	ryz.setBounds(560, 90, 100, 20);
+	ryz.setBounds(280, 60, 100, 20);
 	add(ryz);    
 	    
 	rosol = new JLabel("Rosol: 5");
-	rosol.setBounds(560, 110, 100, 20);
+	rosol.setBounds(280, 75, 100, 20);
 	add(rosol);    
 	    
 	pomidorowa = new JLabel("Pomidorowa: 5");
-	pomidorowa.setBounds(560, 130, 100, 20);
+	pomidorowa.setBounds(280, 90, 100, 20);
 	add(pomidorowa);
 	    
 	kompot = new JLabel("Kompot: 500");
-	kompot.setBounds(560, 150, 100, 20);
+	kompot.setBounds(280, 105, 100, 20);
 	add(kompot);
 	    
 	System.out.println("Model tables " + (model.getTable4Count()+model.getTable4Count()));
-	    
+	
+	
 	for (int i=0;i<model.getTables().size();i++)
 	{
 	  JLabel label = new JLabel("0/" + model.getTables().get(i).getSeatCunt());
 	  tables.add(label);
-	  label.setBounds(100+i*30, 400+20*(i/10), 100, 25);
+	  label.setBounds(120+(i-(i/5)*5)*130, 290+100*(i/5), 100, 25);
 	  add(label);
 	}
+	
 	/*
 	for (int i=0;i<model.getTable2Count();i++)
 	{
@@ -172,7 +186,7 @@ public class AnimPanel extends JPanel implements PropertyChangeListener
   public void propertyChange(PropertyChangeEvent event)
   {
     if (event.getPropertyName().equals("clientQueue"))
-	  clientQueue.setText("Kolejka klientï¿½w: " + model.getClientQueue().size());
+	  clientQueue.setText("Kolejka klientów: " + model.getClientQueue().size());
     if (event.getPropertyName().equals("cookIdleQueue"))
     {
 	  idleCook.setText("Wolni kucharze: " + model.getCookIdleQueue().size());
